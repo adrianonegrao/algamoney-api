c2c69ffaf1a8718aa890f7fcd79a42c3929332c0
adriano <negrao.adriano@gmail.com>
2018-05-28 14:23:22 -0300

    Rest get

---
+++ src/main/java/com/algaworks/algamoney/algamoney/api/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.algaworks.algamoney.algamoney.api.resource;
+
+import com.algaworks.algamoney.algamoney.api.model.Categoria;
+import com.algaworks.algamoney.algamoney.api.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public List<Categoria> listar(){
+        return categoriaRepository.findAll();
+    }
+
+}
